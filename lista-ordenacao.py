b145ef1343c3c4821a5ea846bddec1d106c6ca5b
Marlon Andrei <marlon.andrei@webagencia.com.br>
2024-09-24 16:18:26 -0300

    update

--- lista-ordenacao.py
+++ lista-ordenacao.py
@@ -1,4 +1,5 @@
 my_list = [8, 10, 6, 2, 4]  # Lista para ordenar
+my_list_sort = my_list.copy()
 swapped = True  # É um pouco falso, precisamos dele para entrar no loop while.
 print(my_list)
  
@@ -10,3 +11,10 @@ while swapped:
             my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
 
 print(my_list)
+print(my_list_sort)
+my_list_sort.sort()
+print(my_list_sort)
+
+my_list_reverse = my_list_sort.copy()
+my_list_reverse.reverse()
+print(my_list_reverse)
